bd6f479ede3c74bf5492b6f191f8fdd522b2d7f1
Francesco Romani <fromani@redhat.com>
2019-03-11 18:11:22 +0100

    hvinfo: bootstrap
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hvinfo.go
+++ hvinfo.go
@@ -6,29 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Printf("VendorString:   %s\n", cpuid.VendorIdentificatorString)
-
-	fmt.Printf("Features: ")
-	for i := uint64(0); i < 64; i++ {
-		if cpuid.HasFeature(1 << i) {
-			fmt.Printf("%s ", cpuid.FeatureNames[1<<i])
-		}
-	}
-	fmt.Printf("\n")
-
-	fmt.Printf("ExtendedFeatures: ")
-	for i := uint64(0); i < 64; i++ {
-		if cpuid.HasExtendedFeature(1 << i) {
-			fmt.Printf("%s ", cpuid.ExtendedFeatureNames[1<<i])
-		}
-	}
-	fmt.Printf("\n")
-
-	fmt.Printf("ExtraFeatures: ")
-	for i := uint64(0); i < 64; i++ {
-		if cpuid.HasExtraFeature(1 << i) {
-			fmt.Printf("%s ", cpuid.ExtraFeatureNames[1<<i])
-		}
-	}
-	fmt.Printf("\n")
+	fmt.Printf("VendorString=%s\n", cpuid.VendorIdentificatorString)
+	fmt.Printf("HyperVSupport=%v\n", cpuid.HasHypervSupport())
 }
