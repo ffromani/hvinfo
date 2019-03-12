66e0e06ebdb5b748a097d71be4f4970eba0b5c25
Francesco Romani <fromani@redhat.com>
2019-03-12 09:46:43 +0100

    hvinfo: complete phase 1
    We are now feature complete: we expose all the informations we care
    about.
    We don't cover -nor claim to be- the full 5.0C spec.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # hvinfo
 
 hvinfo dumps the `HyperV` feature support bits as documented in the [HyperV Top Level Functional Specifications](https://github.com/MicrosoftDocs/Virtualization-Documentation/blob/live/tlfs/Hypervisor%20Top%20Level%20Functional%20Specification%20v5.0C.pdf).
-If you don't know what this does mean, you don't need `hvinfo` :)
+
+If you don't know what any of this does mean, you don't need `hvinfo` :)
+
 hvinfo is (C) 2019 Red Hat Inc and licensed under the MIT license.
 
 ## compile
--- hvinfo.go
+++ hvinfo.go
@@ -108,10 +108,70 @@ func GetHypervRecommendations(enabled bool) *HypervRecommendations {
 	}
 }
 
+type HypervNestedFeatures struct {
+	MSRAccessSyNICRegs           bool
+	MSRAccessIntrCtrlRegs        bool
+	MSRAccessHypercallRegs       bool
+	MSRAccessVPIndex             bool
+	MSRAccessReenlighenmentCtrls bool
+	HypercallFastXMMRegister     bool
+	HypercallFastOutput          bool
+	HypercallSintPollingMode     bool
+}
+
+func GetHypervNestedFeatures(enabled bool) *HypervNestedFeatures {
+	if !enabled {
+		return nil
+	}
+	return &HypervNestedFeatures{
+		MSRAccessSyNICRegs:           cpuid.HypervHasNestedFeatureMSR(cpuid.HYPERV_NESTED_FEATURE_MSR_ACCESS_SYNIC_REGS),
+		MSRAccessIntrCtrlRegs:        cpuid.HypervHasNestedFeatureMSR(cpuid.HYPERV_NESTED_FEATURE_MSR_ACCESS_INTR_CTRL_REGS),
+		MSRAccessHypercallRegs:       cpuid.HypervHasNestedFeatureMSR(cpuid.HYPERV_NESTED_FEATURE_MSR_ACCESS_HYPERCALL_MSRS),
+		MSRAccessVPIndex:             cpuid.HypervHasNestedFeatureMSR(cpuid.HYPERV_NESTED_FEATURE_MSR_ACCESS_VPINDEX),
+		MSRAccessReenlighenmentCtrls: cpuid.HypervHasNestedFeatureMSR(cpuid.HYPERV_NESTED_FEATURE_MSR_ACCESS_REENLIGHTENMENT_CTRLS),
+		HypercallFastXMMRegister:     cpuid.HypervHasNestedFeatureHypercall(cpuid.HYPERV_NESTED_FEATURE_HYPERCALL_FAST_XMM_REGISTER),
+		HypercallFastOutput:          cpuid.HypervHasNestedFeatureHypercall(cpuid.HYPERV_NESTED_FEATURE_HYPERCALL_FAST_OUTPUT),
+		HypercallSintPollingMode:     cpuid.HypervHasNestedFeatureHypercall(cpuid.HYPERV_NESTED_FEATURE_HYPERCALL_SINT_POLLING_MODE),
+	}
+}
+
+type HypervNestedOptimizations struct {
+	DirectVirtualFlushHypercall bool
+	HvFlushGuestPhysAddrList    bool
+	EnlightenedMSRBitmap        bool
+}
+
+func GetHypervNestedOptimizations(enabled bool) *HypervNestedOptimizations {
+	if !enabled {
+		return nil
+	}
+	return &HypervNestedOptimizations{
+		DirectVirtualFlushHypercall: cpuid.HypervHasNestedFeatureOptimization(cpuid.HYPERV_NESTED_OPTIMIZATION_DIRECT_VIRTUAL_FLUSH_HYPERCALL),
+		HvFlushGuestPhysAddrList:    cpuid.HypervHasNestedFeatureOptimization(cpuid.HYPERV_NESTED_OPTIMIZATION_HV_FLUSH_GUEST_PHYS_ADDR_LIST),
+		EnlightenedMSRBitmap:        cpuid.HypervHasNestedFeatureOptimization(cpuid.HYPERV_NESTED_OPTIMIZATION_ENLIGHTENED_MSR_BITMAP),
+	}
+}
+
+type HypervNestedInfo struct {
+	Features      *HypervNestedFeatures      `json:",omitempty"`
+	Optimizations *HypervNestedOptimizations `json:",omitempty"`
+}
+
+func GetHypervNestedInfo(enabled bool) *HypervNestedInfo {
+	if !enabled {
+		return nil
+	}
+	return &HypervNestedInfo{
+		Features:      GetHypervNestedFeatures(enabled),
+		Optimizations: GetHypervNestedOptimizations(enabled),
+	}
+}
+
 type HypervInfo struct {
 	HyperVsupport   bool
 	Features        *HypervFeatures        `json:",omitempty"`
 	Recommendations *HypervRecommendations `json:",omitempty"`
+	NestedInfo      *HypervNestedInfo      `json:",omitempty"`
 }
 
 func main() {
@@ -120,6 +180,7 @@ func main() {
 		HyperVsupport:   enabled,
 		Features:        GetHypervFeatures(enabled),
 		Recommendations: GetHypervRecommendations(enabled),
+		NestedInfo:      GetHypervNestedInfo(enabled && cpuid.HypervHasAdditionalNestedEnlightenments()),
 	}
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
